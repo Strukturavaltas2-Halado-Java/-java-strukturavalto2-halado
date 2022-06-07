95d496318e7f02ace40ef848ab6c4abfa2997631
bkris21 <barczaykristof1993@gmail.com>
2022-06-07 15:32:09 +0200

    06-07 lab

---
+++ lab-solutions/used-cars-rest-demo/src/main/java/usedcarsrestdemo/UsedCarsRestDemoApplication.java
@@ -0,0 +1,13 @@
+package usedcarsrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedCarsRestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedCarsRestDemoApplication.class, args);
+    }
+
+}
