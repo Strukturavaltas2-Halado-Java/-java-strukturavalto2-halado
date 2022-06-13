7b4f3f7eea983ba749e0ddc2cbb21d12dedbc0cb
bkris21 <barczaykristof1993@gmail.com>
2022-06-13 16:53:38 +0200

    2022-06-13 lab solution

--- lab-solutions/used-cars-spring-jpa-demo/src/main/java/usedcarsrestdemo/usedcars/repository/CarsRepository.java
+++ lab-solutions/used-cars-spring-jpa-demo/src/main/java/usedcarsrestdemo/usedcars/repository/CarsRepository.java
@@ -1,17 +1,23 @@
 package usedcarsrestdemo.usedcars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import usedcarsrestdemo.usedcars.model.Car;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
-public interface CarsRepository extends JpaRepository<Car,Long> {
+public interface CarsRepository extends JpaRepository<Car, Long> {
 
-    List<Car> findAllByBrand(String brand);
-    List<Car> findAllByAgeIsLessThanEqual(int age);
-    List<Car> findAllByBrandAndAgeIsLessThanEqual(String brand, int age);
+    @Query("select  c from Car c where (:brand is null or c.brand = :brand) and (:age is null or c.age<=:age)")
+    List<Car> findAllByBrandAndAgeIsLessThanEqual(@Param("brand") Optional<String> brand,@Param("age") Optional<Integer> age);
+
+
+    @Query("select c.brand from Car c")
+    Set<String> getAllBrands();
 
 }
--- lab-solutions/used-cars-spring-jpa-demo/src/test/java/usedcarsrestdemo/CarControllerWebClientIT.java
+++ lab-solutions/used-cars-spring-jpa-demo/src/test/java/usedcarsrestdemo/CarControllerWebClientIT.java
@@ -1,25 +1,44 @@
 package usedcarsrestdemo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.zalando.problem.Problem;
+import org.zalando.problem.violations.ConstraintViolationProblem;
 import usedcarsrestdemo.usedcars.dtos.CarDto;
 import usedcarsrestdemo.usedcars.dtos.CreateCarCommand;
+import usedcarsrestdemo.usedcars.dtos.CreateKilometerStateCommand;
+import usedcarsrestdemo.usedcars.dtos.KilometerStateDto;
 import usedcarsrestdemo.usedcars.model.CarCondition;
 
 import java.util.TreeSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(statements = {"delete from car_kilometer_states","delete from cars","delete from car_sellers"})
 class CarControllerWebClientIT {
 
     @Autowired
     WebTestClient webTestClient;
 
+    CarDto carDto;
+
+    @BeforeEach
+    void init(){
+       carDto = webTestClient.post()
+                .uri("api/cars")
+                .bodyValue(new CreateCarCommand("Suzuki", "Swift", 12, CarCondition.EXCELLENT, 123_000))
+                .exchange()
+                .expectBody(CarDto.class)
+                .returnResult().getResponseBody();
+    }
 
     @Test
     void testCreateCar() {
@@ -32,15 +51,7 @@ class CarControllerWebClientIT {
                 .expectBody(CarDto.class)
                 .value(carDto -> assertThat(carDto.getBrand()).isEqualTo("Suzuki"));
 
-        EntityExchangeResult<CarDto> result = webTestClient.post()
-                .uri("api/cars")
-                .bodyValue(new CreateCarCommand("Suzuki", "Swift", 12, CarCondition.EXCELLENT, 123_000))
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody(CarDto.class)
-                .returnResult();
-
-        assertThat(result.getResponseBody().getBrand()).isEqualTo("Suzuki");
+        assertThat(carDto.getBrand()).isEqualTo("Suzuki");
 
     }
 
@@ -84,6 +95,40 @@ class CarControllerWebClientIT {
                 .expectBody(TreeSet.class)
                 .value(t -> assertThat(t).contains("Toyota","Suzuki"));
 
+    }
 
+
+    @Test
+    void testCarNotFound(){
+        Problem p = webTestClient.post()
+                .uri(uriBuilder -> uriBuilder.path("api/cars/{id}/kilometer-states").build(2))
+                .bodyValue(new CreateKilometerStateCommand(60))
+                .exchange()
+                .expectBody(Problem.class).returnResult().getResponseBody();
+
+        assertEquals("Car not found: 2",p.getDetail());
+    }
+
+    @Test
+    void testAddKilometerStateToCar(){
+            CarDto carWithNewKm=webTestClient.post()
+                    .uri(uriBuilder -> uriBuilder.path("api/cars/{id}/kilometer-states").build(carDto.getId()))
+                    .bodyValue(new CreateKilometerStateCommand(125_000))
+                    .exchange()
+                    .expectBody(CarDto.class)
+                    .returnResult().getResponseBody();
+
+            assertThat(carWithNewKm.getKilometerStates()).hasSize(2).extracting(KilometerStateDto::getKm).contains(123_000,125_000);
+
+    }
+
+    @Test
+    void createCarWithWrongKm(){
+        ConstraintViolationProblem cvp = webTestClient.post()
+                .uri("api/cars")
+                .bodyValue(new CreateCarCommand("Suzuki", "Swift", 12, CarCondition.EXCELLENT, -100))
+                .exchange()
+                .expectBody(ConstraintViolationProblem.class).returnResult().getResponseBody();
+        assertEquals("must be positive",cvp.getViolations().get(0).getMessage());
     }
 }
